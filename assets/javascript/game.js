f5e79ebff1c919bf5fc3d21f6ba85e319df9fa5f
Case Western Reserve University <andrew.mancuso@case.edu>
2018-02-22 13:51:57 -0500

    made number generator and click listener

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,35 +1,43 @@
-//$(document).ready(function(){
-//
-var minNumber = 19;
-var maxNumber = 120;
-var compNumber = compNumberFromRange(minNumber, maxNumber);
-//var winCount = 0;
-//var lossCount = 0;
-var yourNumber = 0;
-var wheelValue = Math.floor(Math.random() * 12) + 1;
+$(document).ready(function () {
 
-//generate random number for computer's number
-function compNumberFromRange(min, max) {
-    return Math.floor(Math.random() * (max - min + 1) + min);
-}
-$("#compNumber").text(compNumber);
-//console.log(compNumber);
+    var minNumber = 19;
+    var maxNumber = 120;
+    var compNumber = compNumberFromRange(minNumber, maxNumber);
+    //var winCount = 0;
+    //var lossCount = 0;
+    var yourNumber = 0;
 
-function wheelNum() {
-    ("#wheel1").attr("data-wheelValue", wheelValue);
-    console.log(wheelValue);
-}
+    //generate random number for computer's number
+    function compNumberFromRange(min, max) {
+        return Math.floor(Math.random() * (max - min + 1) + min);
+    }
+    $("#compNumber").text(compNumber);
+    //console.log(compNumber);
 
-    $("#wheel1").on("click", function () {
-        var wheelOne = ($(this).attr("data-wheelValue"));
-        wheelOne = parseInt(wheel1);
-        yourNumber += wheel1;
+    $(".wheel").each(function (i, wheel) {
+        var wheelValue = Math.floor(Math.random() * 12) + 1;
+        $(wheel).attr("data-wheelValue", wheelValue);
+        console.log(wheel, i);
     })
-    $("#yourNumber").text(yourNumber);
+    //.data(wheelValue)
+    //function wheelNum() { 
 
+    //    ("#wheel1").attr("data-wheelValue", wheelValue);
+    //    console.log(wheelValue);
+    //}
+
+    $(".wheel").on("click", function () {
+        var value = $(this).attr("data-wheelValue");
+        value = parseInt(value);
+        yourNumber += value;
+        $("#yourNumber").text(yourNumber);
+        
+    })
+    
+    //wheel.off click listener
 
     //function gameLock(){
     //
     //}
 
-    //});
+});
